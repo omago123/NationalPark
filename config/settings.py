841de9c437aa5d1b94f195b5274a8c4bb578d096
JungKi Kim <73014464+omago123@users.noreply.github.com>
2022-10-16 19:08:53 +0900

    feat: Map update

--- config/settings.py
+++ config/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
 'ENGINE': 'django.db.backends.mysql',
 'NAME': 'mapproject',
 'USER': 'root',
-'PASSWORD': '1234',
+'PASSWORD': '0000',
 'HOST': '127.0.0.1',
 'PORT': '3306',
 }
--- locationApp/urls.py
+++ locationApp/urls.py
@@ -7,12 +7,11 @@ app_name = 'mapApp'
 
 
 urlpatterns = [
-    path('state/', views.state2 , name='mountain'),
-    path('state/main/', views.stateMain , name='state'),
+ 
+    path('state/', views.getState , name='mountain'),
+    path('map/', views.showMap , name='map'),
+    path('map/facility/<int:id>', views.showFacility, name='facility'),
 
-    path('map/', views.map , name='map'),
-    path('map/<int:id>/', views.choice_state , name='map_detail'),
-
-    path('course/create/', views.form_creat , name='creat'),
-    path('state/create/', views.state_creat , name='state_creat'),
+    
 ]
+
